b16aa8841b95283a5c12e7d5a1a1b972866c3702
semiarte <elsemi6@gmail.com>
2025-12-03 12:57:34 +0100

    eager loading and pagination

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $posts = Post::all();
+    $posts = Post::with('author')->paginate(4);
     return view('home', [
         'posts' => $posts
     ]);
